46d0690b60a62f43320c0562a749e83bff365b9b
hasferrr <hasbi.f.hamd@gmail.com>
2022-06-10 08:00:17 +0700

    add 11-modules and 12-string-method

---
+++ function.py
@@ -0,0 +1,13 @@
+def rata2(*angka):
+    jumlah = 0
+    for data in angka:
+        jumlah += data
+    else:
+        average = jumlah / len(angka)
+    print(f'Nilai rata-rata dari {(angka)} adalah {average}')
+
+def total(*angka):
+    jumlah = 0
+    for data in angka:
+        jumlah += data
+    print(f'Total dari {(angka)} adalah {jumlah}')
